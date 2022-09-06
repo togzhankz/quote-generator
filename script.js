a6c355b6ad07298ad7b8365a2b6b2d9c1c3ea847
Togzhan <togjan997@mail.ru>
2022-09-06 10:22:32 +0300

    local api script

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
-// Get quotes from API
 
-let apiQuotes = []
-async function getQuoates() {
-    const apiURL = "https://jacintodesign.github.io/quotes-api/data/quotes.json"
 
-    try {
-        const response = fetch(apiURL)
-        apiQuotes= await response.json()
-        console.log(apiQuotes)
-    } catch(error) {
-        //catch ERROR here
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
-    }
+let apiQuotes = [];
+
+// Loading Spinner
+function mountSpinner() {
+	loader.hidden = false;
+	quoteContainer.hidden = true;
 }
 
-// On load
+// Remove Spinner
+function unmountSpinner() {
+	quoteContainer.hidden = false;
+	loader.hidden = true;
+}
 
-getQuoates()
+// Show New Quote
+function newQuote() {
+	mountSpinner();
+	// Pick a random quote from array
+	const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+	// Check if Author field is blank and replace it with 'Unknown'
+	if (!quote.author) {
+		authorText.textContent = 'Unknown';
+	} else {
+		authorText.textContent = quote.author;
+	}
+	// Check Quote length to determine styling
+	if (quote.text.length > 120) {
+		quoteText.classList.add('long-quote');
+	} else {
+		quoteText.classList.remove('long-quote');
+	}
+	// Set Quote, Hide Loader
+	quoteText.textContent = quote.text;
+	unmountSpinner();
+}
+
+// Get Quotes From API
+async function getQuotes() {
+	mountSpinner();
+	const apiUrl = 'https://type.fit/api/quotes';
+	try {
+		const response = await fetch(apiUrl);
+		apiQuotes = await response.json();
+		newQuote();
+	} catch (error) {
+		// Catch Error Here
+	}
+}
+
+// Tweet Quote
+function tweetQuote() {
+	const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+	window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuotes();
