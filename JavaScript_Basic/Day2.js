f169c6a43248381fc4f5c78c43635d24b54efbc9
Togzhan <togjan997@mail.ru>
2022-09-30 13:19:25 +0300

    35 -36 solutions added

--- JavaScript_Basic/Day2.js
+++ JavaScript_Basic/Day2.js
@@ -1,5 +1,21 @@
 //35. Write a program to check whether a specified character exists within the 2nd to 4th position in a given string.
+
+// const checkFunction = (char, string) => {
+//     shortString = string.substring(1,5)
+//     return shortString.includes(char) 
+// } 
+
+// console.log(checkFunction("n",""))
 //36. Write a JavaScript program to check whether the last digit of the three given positive integers is same.
+// const checkFunction2 = (num1,num2,num3) => {
+//     const last1 = num1%10
+//     const last2 = num2%10
+//     const last3 = num3%10
+//     return (last1 === last2 && last2 === last3)
+
+// }
+
+// console.log(checkFunction2(4,24,34))
 //37. Write a JavaScript program to create new string with first 3 characters are in lower case from a given string. If the string length is less than 3 convert all the characters in upper case. 
 //41. Write a JavaScript program to check three given numbers, if the three numbers are same return 30 otherwise return 20 and if two numbers are same return 40.
 //44. Write a JavaScript program to check from three given integers that whether a number is greater than or equal to 20 and less than one of the others.
