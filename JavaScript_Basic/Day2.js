47e7f0c235b0697b51c846372d9c2aab9c9250d3
Togzhan <togjan997@mail.ru>
2022-09-30 13:43:01 +0300

    37 and  41 solutions added

--- JavaScript_Basic/Day2.js
+++ JavaScript_Basic/Day2.js
@@ -2,8 +2,8 @@
 
 // const checkFunction = (char, string) => {
 //     shortString = string.substring(1,5)
-//     return shortString.includes(char) 
-// } 
+//     return shortString.includes(char)
+// }
 
 // console.log(checkFunction("n",""))
 //36. Write a JavaScript program to check whether the last digit of the three given positive integers is same.
@@ -16,11 +16,38 @@
 // }
 
 // console.log(checkFunction2(4,24,34))
-//37. Write a JavaScript program to create new string with first 3 characters are in lower case from a given string. If the string length is less than 3 convert all the characters in upper case. 
-//41. Write a JavaScript program to check three given numbers, if the three numbers are same return 30 otherwise return 20 and if two numbers are same return 40.
-//44. Write a JavaScript program to check from three given integers that whether a number is greater than or equal to 20 and less than one of the others.
+//37. Write a JavaScript program to create new string with first 3 characters are in lower case from a given string.
+//If the string length is less than 3 convert all the characters in upper case.
+// const checkFunction3 = (string) => {
+//   if (string.length < 3) {
+//     console.log(string.toUpperCase());
+//   } else {
+//     const newString = string.substring(0,3)
+//     const newString2 = string.substring(3)
+//     console.log(`${newString.toLowerCase()}${newString2}`)
+//   }
+
+// };
+
+// checkFunction3("TOGZHAN")
+
+//41. Write a JavaScript program to check three given numbers, 
+//if the three numbers are same return 30 otherwise return 20 and if two numbers are same return 40.
+
+// const checkFunction4 = (num1,num2,num3)=>{
+//     if (num1=== num2 && num2 === num3) return 30
+//     else if (num1 === num2 || num1 === num3 || num2 === num3) return 40
+//     else return 20
+// }
+
+// console.log(checkFunction4(20,20,20))
+
+
+//44. Write a JavaScript program to check from three given integers that whether a number is 
+//greater than or equal to 20 and less than one of the others.
+
 //48. Write a JavaScript program to reverse a given string.
-//49. Write a JavaScript program to replace every character in a given string with the character following it in the alphabet. 
-//50. Write a JavaScript program to capitalize the first letter of each word of a given string. 
-//51. Write a JavaScript program to convert a given number to hours and minutes. 
-//52. Write a JavaScript program to convert the letters of a given string in alphabetical order. 
+//49. Write a JavaScript program to replace every character in a given string with the character following it in the alphabet.
+//50. Write a JavaScript program to capitalize the first letter of each word of a given string.
+//51. Write a JavaScript program to convert a given number to hours and minutes.
+//52. Write a JavaScript program to convert the letters of a given string in alphabetical order.
