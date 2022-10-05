6d83ccc9b9c5956dcf2a9ea8acc311b186de0517
Togzhan <togjan997@mail.ru>
2022-10-05 14:35:50 +0300

    60 solution is added

--- JavaScript_Basic/Day3.js
+++ JavaScript_Basic/Day3.js
@@ -114,6 +114,14 @@ const halfString = (string)=> {
 console.log(halfString("togzhana"))
 // 60. Write a JavaScript program to create a new string without the first and last character of a given string.
 
+const newStringFunction = (string) => {
+  const newString = string.slice(1,-1)
+  return newString
+}
+
+console.log(newStringFunction("togzahn"))
+
+
 // 61. Write a JavaScript program to concatenate two strings except their first character.
 
 // 62. Write a JavaScript program to move last three character to the start of a given string. The string length must be greater or equal to three.
