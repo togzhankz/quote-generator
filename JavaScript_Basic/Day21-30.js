0acc87a38e8eefadc0f77364fc2d39dd3fcc5cda
Togzhan <togjan997@mail.ru>
2022-11-02 15:04:49 +0200

    leetcode problems added

--- JavaScript_Basic/Day21-30.js
+++ JavaScript_Basic/Day21-30.js
@@ -5,5 +5,67 @@
 // https://www.freecodecamp.org/news/javascript-interview-prep-cheatsheet/
 // https://javascript.plainenglish.io/basic-javascript-interview-exercises-c009032aacfc
 // https://www.edureka.co/blog/interview-questions/javascript-interview-questions/
+ 
+
+// Thesis!!! + WORK!!! + 
+
+19.11.2022
+// Leetcode 2 problems per day!
+// Roman to Integer
+// Pascal's Triangle
+
+20.11.2022
+// Leetcode 2 problems per day!
+// Single Number
+// Happy Number
+
+21.11.2022
+// Leetcode 2 problems per day!
+// Missing Number
+// Fizz Buzz
+
+22.11.2022
+// Leetcode 2 problems per day!
+// Reverse String
+// Majority Element
+
+23.11.2022
+// Leetcode 2 problems per day!
+// Valid Anagram
+// Contains Duplicate
+
+24.11.2022
+// Leetcode 2 problems per day!
+// Excel Sheet Column Number
+// First Unique Character in a String
+
+25.11.2022
+// Leetcode 2 problems per day!
+// Longest Common Prefix
+//  Merge Two Sorted Lists
+
+26.11.2022
+// Leetcode 2 problems per day!
+// Plus One
+// Sqrt(x)
 
 
+27.11.2022
+// Leetcode 2 problems per day!
+// Climbing Stairs
+// Binary Tree Inorder Traversal
+
+28.11.2022
+// Leetcode 2 problems per day!
+// Symmetric Tree
+// Maximum Depth of Binary Tree
+
+29.11.2022
+// Leetcode 2 problems per day!
+// Best Time to Buy and Sell Stock
+// Linked List Cycle
+
+30.11.2022
+// Leetcode 2 problems per day!
+// Move Zeroes
+// Add Two Numbers
