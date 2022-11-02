a00ebe0b7ff78de20b8f1104db37d132c3eab5af
Togzhan <togjan997@mail.ru>
2022-11-02 15:54:47 +0200

    interview questions added

--- JavaScript_Basic/Day21-30.js
+++ JavaScript_Basic/Day21-30.js
@@ -69,3 +69,492 @@
 // Leetcode 2 problems per day!
 // Move Zeroes
 // Add Two Numbers
+
+
+DECEMBER
+
+//WORK !!! + THESIS !!! + YELP PROJECT + LEETCODE MEDIUM
+
+01.12.2022 
+3. Longest Substring Without Repeating Characters
+5. Longest Palindromic Substring
+
+02.12.2022
+7. Reverse Integer
+8. String to Integer (atoi)
+
+03.12.2022
+11. Container With Most Water
+15. 3Sum
+
+04.12.2022
+17. Letter Combinations of a Phone Number
+55.6%
+Medium
+19. Remove Nth Node From End of List
+39.9%
+Medium
+
+05.12.2022
+22. Generate Parentheses
+71.8%
+Medium
+28. Find the Index of the First Occurrence in a String
+37.5%
+Medium
+
+06.12.2022
+29. Divide Two Integers
+17.3%
+Medium
+33. Search in Rotated Sorted Array
+38.7%
+Medium
+
+07.12.2022
+34. Find First and Last Position of Element in Sorted Array
+41.5%
+Medium
+36. Valid Sudoku
+56.8%
+Medium
+
+08.12.2022
+38. Count and Say
+51.3%
+Medium
+46. Permutations
+74.7%
+Medium
+
+09.12.2022
+48. Rotate Image
+69.9%
+Medium
+49. Group Anagrams
+66.4%
+Medium
+
+10.12.2022
+50. Pow(x, n)
+32.8%
+Medium
+53. Maximum Subarray
+50.1%
+Medium
+
+11.12.2022
+54. Spiral Matrix
+43.7%
+Medium
+55. Jump Game
+38.4%
+Medium
+
+12.12.2022
+56. Merge Intervals
+45.9%
+Medium
+62. Unique Paths
+62.3%
+Medium
+
+13.12.2022
+73. Set Matrix Zeroes
+50.0%
+Medium
+75. Sort Colors
+57.2%
+Medium
+
+14.12.2022
+78. Subsets
+73.9%
+Medium
+79. Word Search
+39.8%
+Medium
+
+15.12.2022
+91. Decode Ways
+32.2%
+Medium
+98. Validate Binary Search Tree
+31.7%
+Medium
+
+16.12.2022
+102. Binary Tree Level Order Traversal
+63.3%
+Medium
+103. Binary Tree Zigzag Level Order Traversal
+55.1%
+Medium
+
+17.12.2022
+105. Construct Binary Tree from Preorder and Inorder Traversal
+60.7%
+Medium
+116. Populating Next Right Pointers in Each Node
+59.4%
+Medium
+
+18.12.2022
+122. Best Time to Buy and Sell Stock II
+63.3%
+Medium
+128. Longest Consecutive Sequence
+48.9%
+Medium
+
+
+19.12.2022
+130. Surrounded Regions
+35.9%
+Medium
+131. Palindrome Partitioning
+62.4%
+Medium
+
+20.12.2022
+134. Gas Station
+45.1%
+Medium
+138. Copy List with Random Pointer
+50.5%
+Medium
+
+21.12.2022
+139. Word Break
+45.4%
+Medium
+146. LRU Cache
+40.5%
+Medium
+
+22.12.2022
+148. Sort List
+54.1%
+Medium
+150. Evaluate Reverse Polish Notation
+44.1%
+Medium
+
+23.12.2022
+152. Maximum Product Subarray
+34.9%
+Medium
+155. Min Stack
+51.8%
+Medium
+
+24.12.2022
+162. Find Peak Element
+46.2%
+Medium
+166. Fraction to Recurring Decimal
+24.0%
+Medium
+
+25.12.2022
+172. Factorial Trailing Zeroes
+41.7%
+Medium
+179. Largest Number
+34.0%
+Medium
+
+26.12.2022
+189. Rotate Array
+39.2%
+Medium
+198. House Robber
+48.7%
+Medium
+
+27.12.2022
+200. Number of Islands
+56.2%
+Medium
+204. Count Primes
+33.1%
+Medium
+
+28.12.2022
+207. Course Schedule
+45.4%
+Medium
+208. Implement Trie (Prefix Tree)
+60.8%
+Medium
+
+29.12.2022
+210. Course Schedule II
+48.0%
+Medium
+215. Kth Largest Element in an Array
+65.7%
+Medium
+227. Basic Calculator II
+42.2%
+Medium
+
+30.12.2022
+230. Kth Smallest Element in a BST
+69.3%
+Medium
+236. Lowest Common Ancestor of a Binary Tree
+58.0%
+Medium
+237. Delete Node in a Linked List
+75.2%
+
+31.12.2022
+Medium
+238. Product of Array Except Self
+64.7%
+Medium
+240. Search a 2D Matrix II
+
+
+
+JANUARY
+
+// THESIS!!! + WORK!!! + ZTM PROJECT + LEETCODE!!!  (MEDIUM + HARD)
+01.01.2023
+287. Perfect Squares
+287. Find the Duplicate Number
+59.1%
+Medium
+1	What are the possible ways to create objects in JavaScript
+2	What is prototype chain
+3	What is the difference between Call, Apply and Bind
+4	What is JSON and its common operations
+5	What is the purpose of the array slice method
+6	What is the purpose of the array splice method
+7	What is the difference between slice and splice
+8	How do you compare Object and Map
+9	What is the difference between == and === operators
+10	What are lambda or arrow functions
+11	What is a first class function
+12	What is a first order function
+13	What is a higher order function
+14	What is a unary function
+
+
+02.01.2023
+289. Game of Life
+66.7%
+Medium
+300. Longest Increasing Subsequence
+51.6%
+Medium
+
+15	What is the currying function
+16	What is a pure function
+17	What is the purpose of the let keyword
+18	What is the difference between let and var
+19	What is the reason to choose the name let as a keyword
+20	How do you redeclare variables in switch block without an error
+21	What is the Temporal Dead Zone
+22	What is IIFE(Immediately Invoked Function Expression)
+23	How do you decode or encode a URL in JavaScript?
+24	What is memoization
+25	What is Hoisting
+26	What are classes in ES6
+27	What are closures
+28	What are modules
+
+03.01.2023
+322. Coin Change
+41.5%
+Medium
+324. Wiggle Sort II
+32.9%
+Medium
+
+29	Why do you need modules
+30	What is scope in javascript
+31	What is a service worker
+32	How do you manipulate DOM using a service worker
+33	How do you reuse information across service worker restarts
+34	What is IndexedDB
+35	What is web storage
+36	What is a post message
+37	What is a cookie
+38	Why do you need a Cookie
+39	What are the options in a cookie
+40	How do you delete a cookie
+41	What are the differences between cookie, local storage and session storage
+42	What is the main difference between localStorage and sessionStorage
+
+04.01.2023
+328. Odd Even Linked List
+60.2%
+Medium
+334. Increasing Triplet Subsequence
+
+43	How do you access web storage
+44	What are the methods available on session storage
+45	What is a storage event and its event handler
+46	Why do you need web storage
+47	How do you check web storage browser support
+48	How do you check web workers browser support
+49	Give an example of a web worker
+50	What are the restrictions of web workers on DOM
+51	What is a promise
+52	Why do you need a promise
+53	What are the three states of promise
+54	What is a callback function
+55	Why do we need callbacks
+56	What is a callback hell
+57	What are server-sent events
+
+05.01.2023
+341. Flatten Nested List Iterator
+61.5%
+Medium
+347. Top K Frequent Elements
+
+58	How do you receive server-sent event notifications
+59	How do you check browser support for server-sent events
+60	What are the events available for server sent events
+61	What are the main rules of promise
+62	What is callback in callback
+63	What is promise chaining
+64	What is promise.all
+65	What is the purpose of the race method in promise
+66	What is a strict mode in javascript
+67	Why do you need strict mode
+68	How do you declare strict mode
+69	What is the purpose of double exclamation
+70	What is the purpose of the delete operator
+
+06.01.2023
+371. Sum of Two Integers
+50.7%
+Medium
+378. Kth Smallest Element in a Sorted Matrix
+61.6%
+Medium
+
+71	What is typeof operator
+72	What is undefined property
+73	What is null value
+74	What is the difference between null and undefined
+75	What is eval
+76	What is the difference between window and document
+77	How do you access history in javascript
+78	How do you detect caps lock key turned on or not
+79	What is isNaN
+80	What are the differences between undeclared and undefined variables
+81	What are global variables
+82	What are the problems with global variables
+83	What is NaN property
+84	What is the purpose of isFinite function
+
+07.01.2023
+380. Insert Delete GetRandom O(1)
+52.0%
+Medium
+384. Shuffle an Array
+57.7%
+Medium
+85	What is an event flow
+86	What is event bubbling
+87	What is event capturing
+88	How do you submit a form using JavaScript
+89	How do you find operating system details
+90	What is the difference between document load and DOMContentLoaded events
+91	What is the difference between native, host and user objects
+92	What are the tools or techniques used for debugging JavaScript code
+93	What are the pros and cons of promises over callbacks
+94	What is the difference between an attribute and a property
+95	What is same-origin policy
+96	What is the purpose of void 0
+97	Is JavaScript a compiled or interpreted language
+98	Is JavaScript a case-sensitive language
+
+08.01.2023
+395. Longest Substring with At Least K Repeating Characters
+44.8%
+Medium
+454. 4Sum II
+
+09.01.2023
+4. Median of Two Sorted Arrays
+35.2%
+Hard
+10. Regular Expression Matching
+28.2%
+Hard
+10.01.2023
+23. Merge k Sorted Lists
+48.4%
+Hard
+41. First Missing Positive
+36.5%
+Hard
+11.01.2023
+42. Trapping Rain Water
+58.8%
+Hard
+44. Wildcard Matching
+26.8%
+Hard
+12.01.2023
+
+76. Minimum Window Substring
+40.7%
+Hard
+13.01.2023
+84. Largest Rectangle in Histogram
+42.1%
+Hard
+14.01.2023
+124. Binary Tree Maximum Path Sum
+38.4%
+Hard
+15.01.2023
+127. Word Ladder
+36.7%
+Hard
+16.01.2023
+140. Word Break II
+44.4%
+Hard
+17.01.2023
+149. Max Points on a Line
+21.7%
+Hard
+18.01.2023
+212. Word Search II
+36.9%
+Hard
+19.01.2023
+218. The Skyline Problem
+41.6%
+Hard
+20.01.2023
+239. Sliding Window Maximum
+
+
+21.01.2023
+
+
+22.01.2023
+23.01.2023
+24.01.2023
+25.01.2023
+26.01.2023
+27.01.2023
+28.01.2023
+29.01.2023
+30.01.2023
+31.01.2023
+
+
+
+
+
+
