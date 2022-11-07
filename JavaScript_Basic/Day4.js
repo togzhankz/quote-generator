1d1e29c408f405f5e018a587da15c44d1e7c2889
Togzhan <togjan997@mail.ru>
2022-11-07 22:25:34 +0200

    added solution to 121

--- JavaScript_Basic/Day4.js
+++ JavaScript_Basic/Day4.js
@@ -408,6 +408,25 @@ console.log(isInsideCircle(3,2,3,6,2))
 // 121. Write a JavaScript program to check whether a given matrix is lower triangular or not. 
 // Note: A square matrix is called lower triangular if all the entries above the main diagonal are zero.
 
+const isTriangular = (matrix) => {
+    for(let i = 0; i< matrix.length; i++){
+        for(let j = i+1 ; j < matrix.length; j++){
+            if(i<j && matrix[i][j]!==0)
+            return false
+        }
+    }
+    return true
+}
+
+console.log(isTriangular(
+    [
+        [1,0,0,0],
+        [1,1,0,0],
+        [2,3,2,0],
+        [1,2,4,4]
+    ]
+))
+
 
 // 122. Write a JavaScript program to check whether a given array of integers represents either a strictly increasing or a strictly decreasing sequence. 
 
