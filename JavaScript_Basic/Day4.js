ce51d1a5661abf8887fbe9cb6b87635be738ff45
Togzhan <togjan997@mail.ru>
2022-11-09 14:48:14 +0200

    added solution to 131

--- JavaScript_Basic/Day4.js
+++ JavaScript_Basic/Day4.js
@@ -573,11 +573,26 @@ const even_digits = (num)=> {
   console.log(even_digits(12883));
 
 // 131. Write a JavaScript program to create an array of prefix sums of the given array. 
-// In computer science, the prefix sum, cumulative sum, inclusive scan, or simply scan of a sequence of numbers x0, x1, x2, ... is a second sequence of numbers y0, y1, y2, ..., the sums of prefixes of the input sequence:
+// In computer science, the prefix sum, cumulative sum, inclusive scan, or simply scan of a sequence of numbers 
+//x0, x1, x2, ... is a second sequence of numbers y0, y1, y2, ..., the sums of prefixes of the input sequence:
 // y0 = x0
 // y1 = x0 + x1
 // y2 = x0 + x1+ x2
 // ...
+
+const prefixSum = (arr) => {
+    const new_arr = [];
+for (let i = 0; i < arr.length; i++) {
+  new_arr[i] = 0;
+  for (let j = 0; j < i + 1; j++) {
+    new_arr[i] += arr[j];
+  }
+}
+return new_arr;
+}
+
+console.log(prefixSum([1,2,3,5,99]))
+
 
 
 // 132. Write a JavaScript program to find all distinct prime factors of a given integer. 
