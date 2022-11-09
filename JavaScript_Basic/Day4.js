711e66a6d93953e1ec2cdeed09e5674b43459f05
Togzhan <togjan997@mail.ru>
2022-11-09 18:37:49 +0200

    added solution to 134

--- JavaScript_Basic/Day4.js
+++ JavaScript_Basic/Day4.js
@@ -635,7 +635,21 @@ const proper_improper_test = (num) => {
   console.log(proper_improper_test([104, 2]));
   console.log(proper_improper_test([5, 40]));
 
-// 134. Write a JavaScript program to change the characters (lower case) in a string where a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a. 
+// 134. Write a JavaScript program to change the characters (lower case) in a string
+// where a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a. 
+const  change_char = (str1) => {
+	const result = [];
+	for (let i = 0; i < str1.length; i++)
+    {
+        const char_order = str1.charCodeAt(i) - 'a'.charCodeAt(0);
+        const change_char = 25 - char_order + 'a'.charCodeAt(0);
+        result.push(String.fromCharCode(change_char));
+    }
+	return result.join("");
+}
+console.log(change_char("abcxyz"));
+console.log(change_char("python")); 
+
 
 
 // 135. Write a JavaScript program to remove all characters from a given string that appear more than once. 
