8dd55269cad2ae04e8c59331782104cb4a5c46d0
Togzhan <togjan997@mail.ru>
2022-11-09 15:02:52 +0200

    added solution to 133

--- JavaScript_Basic/Day4.js
+++ JavaScript_Basic/Day4.js
@@ -620,8 +620,20 @@ const  prime_factors = (num)=> {
   console.log(prime_factors(101));
 
 // 133. Write a JavaScript program to check whether a given fraction is proper or not. 
-// Note: There are two types of common fractions, proper or improper. When the numerator and the denominator are both positive, the fraction is called proper if the numerator is less than the denominator, and improper otherwise.
+// Note: There are two types of common fractions, proper or improper. When the numerator and the denominator are 
+//both positive, the fraction is called proper if the numerator is less than the denominator, and improper otherwise.
 
+
+const proper_improper_test = (num) => {
+    return Math.abs(num[0] / num[1]) < 1
+      ? "Proper fraction."
+      : "Improper fraction.";
+  }
+  console.log(proper_improper_test([12, 300]));
+  console.log(proper_improper_test([2, 4]));
+  console.log(proper_improper_test([103, 3]));
+  console.log(proper_improper_test([104, 2]));
+  console.log(proper_improper_test([5, 40]));
 
 // 134. Write a JavaScript program to change the characters (lower case) in a string where a turns into z, b turns into y, c turns into x, ..., n turns into m, m turns into n, ..., z turns into a. 
 
