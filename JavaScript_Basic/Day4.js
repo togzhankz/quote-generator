8261bb787db23e28385cd89d882269bf1ee2cf80
Togzhan <togjan997@mail.ru>
2022-11-13 19:57:52 +0200

    added solution to 150

--- JavaScript_Basic/Day4.js
+++ JavaScript_Basic/Day4.js
@@ -897,3 +897,21 @@ const capitalizationString = (str) => {
 console.log(capitalizationString("aPPa"));
 
 // 150. Write a JavaScript program to swap pairs of adjacent digits of a given integer of even length`.
+
+
+function swap_adjacent_digits(n) {  
+    var result = 0,
+        x = 1;
+  
+    while (n != 0) {
+      var dg1 = n % 10,
+          dg2 = (n - dg1) / 10 % 10;
+      result += x * (10 * dg1 + dg2);
+      n = Math.floor(n / 100);
+      x *= 100;
+    }
+  
+    return result;
+  }
+  
+  console.log(swap_adjacent_digits(15));
